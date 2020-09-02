6a927ecd04306651badab90f5980d104a19a5a61
Ramandeep Singh <ramandeepsingh@Ramandeeps-MacBook-Pro-2.local>
2020-09-01 21:29:43 -0400

    React router setup

--- src/app/index.js
+++ src/app/index.js
@@ -5,6 +5,13 @@ import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 import rootReducer from '../reducers'
 import TestComponent from './TestComponent';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Page404 from './Page404/Page404';
 
 const store = createStore(rootReducer, applyMiddleware(thunk, logger))
 window.store = store
@@ -12,7 +19,12 @@ window.store = store
 function App() {
   return (
     <Provider store={store}>
-      <TestComponent />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={TestComponent} />
+          <Route path='/*' component={Page404} />
+        </Switch>
+      </Router>
     </Provider>
   );
 }
